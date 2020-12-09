7047e0b0e022459b0fb7b8ba139f69f6c20409f3
Yutao <yutaogui@gmail.com>
2020-12-09 22:53:44 +0100

    Driver code to perform simulation

--- hanabi_sim/main.py
+++ hanabi_sim/main.py
@@ -3,6 +3,9 @@ import logging
 import hanasim as hs
 import cheatingAgent as agent
 
+import pandas as pd
+import numpy as np
+
 # Number of cards each player has in their hand
 HANDSIZE = {2: 5,
             3: 5,
@@ -31,17 +34,25 @@ game.setup()
 player1 = agent.Agent(0, game)
 player2 = agent.Agent(1, game)
 
-nTurn = 0
-while (game.isOver is False):
-    playerToPlay = nTurn % nPlayers
+N = 2000
+scores = np.zeros(N)
+for i in range(N):
+    nTurn = 0
+    while (not game.isOver):
+        playerToPlay = nTurn % nPlayers
 
-    if playerToPlay == 0:
-        move = player1.findMove()
-    else:
-        move = player2.findMove()
+        if playerToPlay == 0:
+            move = player1.findMove()
+        else:
+            move = player2.findMove()
 
-    game.doMove(move)
+        game.doMove(move)
 
-print("The final score is: {}".format(game.score))
+    scores[i] = game.score;
+    game.reset()
+    game.setup()
 
+df = pd.DataFrame({'Scores' : scores})
+print(df.describe())
+hist = df.plot.hist(bins=10)
 

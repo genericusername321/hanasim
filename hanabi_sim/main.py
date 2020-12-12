86014d072c939062b154387473dafd7c7091a92f
Yutao <yutaogui@gmail.com>
2020-12-12 01:57:29 +0100

    Update main.py with timer functionality

--- hanabi_sim/main.py
+++ hanabi_sim/main.py
@@ -1,5 +1,6 @@
 import pytest
 import logging
+import time
 import hanasim as hs
 import cheatingAgent as agent
 
@@ -12,8 +13,7 @@ HANDSIZE = {2: 5,
             4: 4,
             5: 4}
 
-logLevel = logging.INFO
-
+logLevel = logging.CRITICAL 
 formatter = logging.Formatter('%(name)s:%(levelname)s:%(message)s')
 fh = logging.FileHandler(filename='hanabi.log', mode='w')
 fh.setLevel(logLevel)
@@ -33,21 +33,26 @@ game.setup()
 # Set up players
 players = [agent.Agent(ii, game) for ii in range(nPlayers)]
 
-N = 2
+N = 10000
 scores = np.zeros(N)
+times = np.zeros(N)
 for i in range(N):
     ply = 0
+    tic = time.perf_counter()
     while (not game.isOver):
         turn = ply % nPlayers
         ply=ply+1
         move = players[turn].findMove()
         game.doMove(move)
-
+    toc = time.perf_counter()
     scores[i] = game.score;
+    times[i] = toc-tic
     game.reset()
     game.setup()
 
 df = pd.DataFrame({'Scores' : scores})
 print(df.describe())
-hist = df.plot.hist(bins=10)
+
+dfTime = pd.DataFrame({'Time' : times})
+print(dfTime.describe())
 
